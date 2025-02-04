b6b791b7fdc900d8d123f123594c1ba65ef285e8
Dangabit <ericgohky@gmail.com>
2025-02-04 13:40:24 +0800

    refactor(Randomly): Replace conditionals with Guard Clause: Create 2 guards clause instead of if-else
    Note: I expect Randomly.getBoolean() to produce a boolean randomly, hence toUpperCase is not necessarily unreachable. Therefore I split into guard clauses instead (may still be flagged out).

--- src/sqlancer/Randomly.java
+++ src/sqlancer/Randomly.java
@@ -295,17 +295,18 @@ public final class Randomly {
             public String transformCachedString(Randomly r, String randomString) {
                 if (Randomly.getBoolean()) {
                     return randomString.toLowerCase();
-                } else if (Randomly.getBoolean()) {
-                    return randomString.toUpperCase();
-                } else {
-                    char[] chars = randomString.toCharArray();
-                    if (chars.length != 0) {
-                        for (int i = 0; i < Randomly.smallNumber(); i++) {
-                            chars[r.getInteger(0, chars.length)] = ALPHABET.charAt(r.getInteger(0, ALPHABET.length()));
-                        }
-                    }
-                    return new String(chars);
                 }
+                if (Randomly.getBoolean()) {
+                    return randomString.toUpperCase();
+                } 
+
+                char[] chars = randomString.toCharArray();
+                if (chars.length != 0) {
+                    for (int i = 0; i < Randomly.smallNumber(); i++) {
+                        chars[r.getInteger(0, chars.length)] = ALPHABET.charAt(r.getInteger(0, ALPHABET.length()));
+                    }
+                }
+                return new String(chars);
             }
 
         };
