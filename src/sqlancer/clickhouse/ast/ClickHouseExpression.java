f077d92c25a27eec5b6807b909f138a840a912a5
Dangabit <ericgohky@gmail.com>
2025-02-05 17:42:42 +0800

    refactor(ClickHouseExpression): Remove Comments: Removed commented out codes

--- src/sqlancer/clickhouse/ast/ClickHouseExpression.java
+++ src/sqlancer/clickhouse/ast/ClickHouseExpression.java
@@ -17,7 +17,6 @@ public abstract class ClickHouseExpression implements Expression<ClickHouseColum
         NOTHING, UINT8, UINT16, UINT32, UINT64, UINT128, INT8, INT16, INT32, INT64, INT128, FLOAT32, FLOAT64, DATE,
         DATETIME, DATETIME64, STRING, FIXEDSTRING, ENUM8, ENUM16, DECIMAL32, DECIMAL64, DECIMAL128, UUID, ARRAY, TUPLE,
         SET, INTERVAL;
-        // NULLABLE, FUNCTION, AGGREGATEFUNCTION, LOWCARDINALITY;
 
         public boolean isNumeric() {
             return this == UINT8 || this == UINT16 || this == UINT32 || this == UINT64 || this == UINT128
