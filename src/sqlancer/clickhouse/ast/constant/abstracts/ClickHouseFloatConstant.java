287dc07dc40fbb839069e09e7a54f680a80f584c
Jeremy <e1155908@u.nus.edu>
2025-02-21 18:27:55 +0800

    refactor(ClickHouseFloatConstant): Extract abstract class: To remove dupe lines

---
+++ src/sqlancer/clickhouse/ast/constant/abstracts/ClickHouseFloatConstant.java
@@ -0,0 +1,133 @@
+package sqlancer.clickhouse.ast.constant.abstracts;
+
+import com.clickhouse.client.ClickHouseDataType;
+import sqlancer.clickhouse.ast.ClickHouseConstant;
+import sqlancer.clickhouse.ast.constant.ClickHouseCreateConstant;
+
+import java.math.BigInteger;
+
+public abstract class ClickHouseFloatConstant<T extends Number> extends ClickHouseConstant {
+
+    protected final T value;
+
+    protected ClickHouseFloatConstant(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public Object getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean isNull() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        if (value.doubleValue() == Double.POSITIVE_INFINITY) {
+            return "'+Inf'";
+        } else if (value.doubleValue() == Double.NEGATIVE_INFINITY) {
+            return "'-Inf'";
+        }
+        return String.valueOf(value);
+    }
+
+    @Override
+    public boolean compareInternal(Object val) {
+        return Double.compare(value.doubleValue(), ((Number) val).doubleValue()) == 0;
+    }
+
+    @Override
+    public ClickHouseConstant applyLess(ClickHouseConstant right) {
+        if (this.getDataType() == right.getDataType()) {
+            return this.asDouble() < right.asDouble() ? ClickHouseCreateConstant.createTrue()
+                    : ClickHouseCreateConstant.createFalse();
+        }
+        ClickHouseConstant converted = right.cast(ClickHouseDataType.Float32);
+        return this.asDouble() < converted.asDouble() ? ClickHouseCreateConstant.createTrue()
+                : ClickHouseCreateConstant.createFalse();
+    }
+
+    @Override
+    public boolean asBooleanNotNull() {
+        return Double.compare(value.doubleValue(), 0.0) == 0;
+    }
+
+    @Override
+    public double asDouble() {
+        return value.doubleValue();
+    }
+
+    @Override
+    public ClickHouseConstant cast(ClickHouseDataType type) {
+        switch (type) {
+            case String:
+                return ClickHouseCreateConstant.createStringConstant(this.toString());
+            case UInt8:
+                return ClickHouseCreateConstant.createUInt8Constant((long) value);
+            case Int8:
+                return ClickHouseCreateConstant.createInt8Constant((long) value);
+            case UInt16:
+                return ClickHouseCreateConstant.createUInt16Constant((long) value);
+            case Int16:
+                return ClickHouseCreateConstant.createInt16Constant((long) value);
+            case UInt32:
+                return ClickHouseCreateConstant.createUInt32Constant((long) value);
+            case Int32:
+                return ClickHouseCreateConstant.createInt32Constant((long) value);
+            case UInt64:
+                return ClickHouseCreateConstant.createUInt64Constant(BigInteger.valueOf((long) value));
+            case Int64:
+                return ClickHouseCreateConstant.createInt64Constant(BigInteger.valueOf((long) value));
+            case Float32:
+                return ClickHouseCreateConstant.createFloat32Constant(value.floatValue());
+            case Float64:
+                return ClickHouseCreateConstant.createFloat64Constant(value.doubleValue());
+            case Nothing:
+                return ClickHouseCreateConstant.createNullConstant();
+            case Bool:
+                return ClickHouseCreateConstant.createBooleanConstant(value.doubleValue() != 0);
+            case IntervalYear:
+            case IntervalQuarter:
+            case IntervalMonth:
+            case IntervalWeek:
+            case IntervalDay:
+            case IntervalHour:
+            case IntervalMinute:
+            case IntervalSecond:
+            case Date:
+            case Date32:
+            case DateTime:
+            case DateTime32:
+            case DateTime64:
+            case Decimal:
+            case Decimal32:
+            case Decimal64:
+            case Decimal128:
+            case Decimal256:
+            case UUID:
+            case Enum:
+            case Enum8:
+            case Enum16:
+            case IPv4:
+            case IPv6:
+            case FixedString:
+            case AggregateFunction:
+            case SimpleAggregateFunction:
+            case Array:
+            case Map:
+            case Nested:
+            case Tuple:
+            case Point:
+            case Polygon:
+            case MultiPolygon:
+            case Ring:
+            default:
+                throw new AssertionError(type);
+        }
+    }
+
+    public abstract ClickHouseDataType getDataType();
+}
