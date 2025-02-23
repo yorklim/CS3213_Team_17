13dcb96012ffd120a11455f460edd65af7c02c74
Zhai Yuxin <e0772744@u.nus.edu>
2025-02-23 20:06:00 +0800

    refactor(Materialize): Remove duplicates: Extract getJoinStatements to MaterializeCommon getCommonJoinStatements.

--- src/sqlancer/materialize/gen/MaterializeExpressionGenerator.java
+++ src/sqlancer/materialize/gen/MaterializeExpressionGenerator.java
@@ -21,7 +21,6 @@ import sqlancer.materialize.MaterializeSchema.MaterializeColumn;
 import sqlancer.materialize.MaterializeSchema.MaterializeDataType;
 import sqlancer.materialize.MaterializeSchema.MaterializeRowValue;
 import sqlancer.materialize.MaterializeSchema.MaterializeTable;
-import sqlancer.materialize.MaterializeSchema.MaterializeTables;
 import sqlancer.materialize.ast.MaterializeAggregate;
 import sqlancer.materialize.ast.MaterializeAggregate.MaterializeAggregateFunction;
 import sqlancer.materialize.ast.MaterializeBetweenOperation;
@@ -42,7 +41,6 @@ import sqlancer.materialize.ast.MaterializeFunction.MaterializeFunctionWithResul
 import sqlancer.materialize.ast.MaterializeFunctionWithUnknownResult;
 import sqlancer.materialize.ast.MaterializeInOperation;
 import sqlancer.materialize.ast.MaterializeJoin;
-import sqlancer.materialize.ast.MaterializeJoin.MaterializeJoinType;
 import sqlancer.materialize.ast.MaterializeLikeOperation;
 import sqlancer.materialize.ast.MaterializeOrderByTerm;
 import sqlancer.materialize.ast.MaterializeOrderByTerm.MaterializeOrder;
@@ -55,9 +53,7 @@ import sqlancer.materialize.ast.MaterializePrefixOperation;
 import sqlancer.materialize.ast.MaterializePrefixOperation.PrefixOperator;
 import sqlancer.materialize.ast.MaterializeSelect;
 import sqlancer.materialize.ast.MaterializeSelect.MaterializeFromTable;
-import sqlancer.materialize.ast.MaterializeSelect.MaterializeSubquery;
 import sqlancer.materialize.ast.MaterializeSelect.SelectType;
-import sqlancer.materialize.oracle.tlp.MaterializeTLPBase;
 
 public class MaterializeExpressionGenerator implements ExpressionGenerator<MaterializeExpression>,
         NoRECGenerator<MaterializeSelect, MaterializeJoin, MaterializeExpression, MaterializeTable, MaterializeColumn>,
@@ -553,29 +549,7 @@ public class MaterializeExpressionGenerator implements ExpressionGenerator<Mater
 
     @Override
     public List<MaterializeJoin> getRandomJoinClauses() {
-        List<MaterializeJoin> joinStatements = new ArrayList<>();
-        MaterializeExpressionGenerator gen = new MaterializeExpressionGenerator(globalState).setColumns(columns);
-        for (int i = 1; i < tables.size(); i++) {
-            MaterializeExpression joinClause = gen.generateExpression(MaterializeDataType.BOOLEAN);
-            MaterializeTable table = Randomly.fromList(tables);
-            tables.remove(table);
-            MaterializeJoinType options = MaterializeJoinType.getRandom();
-            MaterializeJoin j = new MaterializeJoin(new MaterializeFromTable(table, Randomly.getBoolean()), joinClause,
-                    options);
-            joinStatements.add(j);
-        }
-        // JOIN subqueries
-        for (int i = 0; i < Randomly.smallNumber(); i++) {
-            MaterializeTables subqueryTables = globalState.getSchema().getRandomTableNonEmptyTables();
-            MaterializeSubquery subquery = MaterializeTLPBase.createSubquery(globalState, String.format("sub%d", i),
-                    subqueryTables);
-            MaterializeExpression joinClause = gen.generateExpression(MaterializeDataType.BOOLEAN);
-            MaterializeJoinType options = MaterializeJoinType.getRandom();
-            MaterializeJoin j = new MaterializeJoin(subquery, joinClause, options);
-            joinStatements.add(j);
-        }
-
-        return joinStatements;
+        return MaterializeCommon.getCommonJoinStatements(globalState, columns, tables);
     }
 
     @Override
