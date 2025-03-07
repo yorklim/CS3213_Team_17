6d8f31c36cd8ae03916678ec3f0bea0d31e2873a
yorklim <yorklim1016@gmail.com>
2025-03-07 15:41:12 +0800

    Fix bug

--- src/sqlancer/common/TableCreator.java
+++ src/sqlancer/common/TableCreator.java
@@ -1,18 +1,16 @@
 package sqlancer.common;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import sqlancer.SQLGlobalState;
 import sqlancer.common.query.SQLQueryAdapter;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
 public abstract class TableCreator {
 
     public abstract void create() throws Exception;
 
-    public static void runQueryFromFile(String file, SQLGlobalState<?,?> globalState) {
+    public static void runQueryFromFile(String file, SQLGlobalState<?, ?> globalState) {
         try {
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
@@ -23,12 +21,7 @@ public abstract class TableCreator {
             }
             br.close();
             fr.close();
-        } catch (FileNotFoundException e) {
-
-        } catch (IOException e) {
-
         } catch (Exception e) {
-
         }
     }
 }
