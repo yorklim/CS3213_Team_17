1e8a41c8367701480d0571f1cbe34fd8a04d2ff4
Dangabit <ericgohky@gmail.com>
2025-03-08 10:36:42 +0800

    Refactor(OceanBaseProvider): Pull out Oceanbase Table Creation: Created and extended from TableCreator & TableQueryGenerator, removed statement executor

---
+++ src/sqlancer/oceanbase/OceanBaseTableCreator.java
@@ -0,0 +1,63 @@
+package sqlancer.oceanbase;
+
+import sqlancer.IgnoreMeException;
+import sqlancer.Randomly;
+import sqlancer.common.DBMSCommon;
+import sqlancer.common.TableCreator;
+import sqlancer.common.query.SQLQueryAdapter;
+import sqlancer.oceanbase.gen.OceanBaseTableGenerator;
+
+public class OceanBaseTableCreator extends TableCreator {
+    private final OceanBaseGlobalState globalState;
+
+    public OceanBaseTableCreator(OceanBaseGlobalState globalState) {
+        this.globalState = globalState;
+    }
+
+    private void createTable() throws Exception {
+        for (int i = 0; i < Randomly.smallNumber() + 1; i++) {
+            boolean success = false;
+            do {
+                try {
+                    String tableName = DBMSCommon.createTableName(globalState.getSchema().getDatabaseTables().size());
+                    SQLQueryAdapter createTable = OceanBaseTableGenerator.generate(globalState, tableName);
+                    success = globalState.executeStatement(createTable);
+                } catch (IgnoreMeException e) {
+
+                }
+            } while (!success);
+        }
+    }
+
+    @Override
+    public void create() throws Exception {
+        createTable();
+
+        OceanBaseTableQueryGenerator generator = new OceanBaseTableQueryGenerator(globalState);
+        generator.generate();
+
+        while (!generator.isFinished()) {
+            OceanBaseTableQueryGenerator.Action nextAction = generator.getRandNextAction();
+            assert nextAction != null;
+            SQLQueryAdapter query = null;
+            try {
+                boolean success = false;
+                int nrTries = 0;
+                do {
+                    query = nextAction.getQuery(globalState);
+                    success = globalState.executeStatement(query);
+                } while (nextAction.canBeRetried() && !success
+                        && nrTries++ < globalState.getOptions().getNrStatementRetryCount());
+            } catch (IgnoreMeException e) {
+
+            }
+            if (query != null && query.couldAffectSchema()) {
+                globalState.updateSchema();
+                throw new IgnoreMeException();
+            }
+            if (globalState.getSchema().getDatabaseTables().isEmpty()) {
+                throw new IgnoreMeException();
+            }
+        }
+    }
+}
