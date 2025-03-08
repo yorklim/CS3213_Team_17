1ab0d06e679431b81bd59a90b4f415c4387dab3b
Dangabit <ericgohky@gmail.com>
2025-03-09 01:27:51 +0800

    Refactor<yugabyte.ysql>: Pull Out Table Creation: Created & extended from TableCreator & TableQueryGenerator, removed statement executor

---
+++ src/sqlancer/yugabyte/ysql/YSQLTableCreator.java
@@ -0,0 +1,72 @@
+package sqlancer.yugabyte.ysql;
+
+import sqlancer.IgnoreMeException;
+import sqlancer.Randomly;
+import sqlancer.common.DBMSCommon;
+import sqlancer.common.TableCreator;
+import sqlancer.common.query.SQLQueryAdapter;
+import sqlancer.yugabyte.ysql.gen.YSQLTableGenerator;
+
+public class YSQLTableCreator extends TableCreator {
+    private final YSQLGlobalState globalState;
+    private final boolean generateOnlyKnown;
+
+    public YSQLTableCreator(YSQLGlobalState globalState, boolean generateOnlyKnown) {
+        this.globalState = globalState;
+        this.generateOnlyKnown = generateOnlyKnown;
+    }
+
+    private void createTable() throws Exception {
+        synchronized (YSQLProvider.DDL_LOCK) {
+            for (int i = 0; i < Randomly.fromOptions(4, 5, 6); i++) {
+                boolean success = false;
+                do {
+                    try {
+                        String tableName = DBMSCommon
+                                .createTableName(globalState.getSchema().getDatabaseTables().size());
+                        SQLQueryAdapter createTable = YSQLTableGenerator.generate(tableName, generateOnlyKnown,
+                                globalState);
+                        success = globalState.executeStatement(createTable);
+                    } catch (IgnoreMeException e) {
+                        YSQLProvider.exceptionLessSleep(5000);
+                    }
+                } while (!success);
+            }
+        }
+    }
+
+    @Override
+    public void create() throws Exception {
+        createTable();
+
+        YSQLTableQueryGenerator generator = new YSQLTableQueryGenerator(globalState);
+        generator.generate();
+
+        while (!generator.isFinished()) {
+            YSQLTableQueryGenerator.Action nextAction = generator.getRandNextAction();
+            assert nextAction != null;
+            SQLQueryAdapter query = null;
+            try {
+                boolean success = false;
+                int nrTries = 0;
+                do {
+                    query = nextAction.getQuery(globalState);
+                    success = globalState.executeStatement(query);
+                } while (nextAction.canBeRetried() && !success
+                        && nrTries++ < globalState.getOptions().getNrStatementRetryCount());
+            } catch (IgnoreMeException e) {
+
+            }
+            if (query != null && query.couldAffectSchema()) {
+                globalState.updateSchema();
+                throw new IgnoreMeException();
+            }
+            if (globalState.getSchema().getDatabaseTables().isEmpty()) {
+                throw new IgnoreMeException();
+            }
+        }
+
+        globalState.executeStatement(new SQLQueryAdapter("COMMIT", true));
+        globalState.executeStatement(new SQLQueryAdapter("SET SESSION statement_timeout = 15000;\n"));
+    }
+}
