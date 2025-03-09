e6218d82a73298c5ff4c74250869b2674e6d6c3a
Jeremy <e1155908@u.nus.edu>
2025-03-09 17:02:22 +0800

    refactor(CnosDBProvider): Extract function:
    Extacted out the logic of generateDatabase method into
    CnosDBTableQueryGenerator and CnosDBTableCreator classes.
    Additionally, the Actions enum is also moved into the
    CnosDBTableQueryGenerator class to encapsulate all the actions that is
    required to generate the queries for table creation.

--- src/sqlancer/cnosdb/CnosDBProvider.java
+++ src/sqlancer/cnosdb/CnosDBProvider.java
@@ -1,21 +1,11 @@
 package sqlancer.cnosdb;
 
-import java.util.Objects;
-
 import com.google.auto.service.AutoService;
 
-import sqlancer.AbstractAction;
 import sqlancer.DatabaseProvider;
-import sqlancer.IgnoreMeException;
 import sqlancer.ProviderAdapter;
-import sqlancer.Randomly;
-import sqlancer.StatementExecutor;
 import sqlancer.cnosdb.client.CnosDBClient;
 import sqlancer.cnosdb.client.CnosDBConnection;
-import sqlancer.cnosdb.gen.CnosDBInsertGenerator;
-import sqlancer.cnosdb.gen.CnosDBTableGenerator;
-import sqlancer.cnosdb.query.CnosDBOtherQuery;
-import sqlancer.cnosdb.query.CnosDBQueryProvider;
 import sqlancer.common.log.LoggableFactory;
 
 @AutoService(DatabaseProvider.class)
@@ -35,27 +25,15 @@ public class CnosDBProvider extends ProviderAdapter<CnosDBGlobalState, CnosDBOpt
         super(globalClass, optionClass);
     }
 
-    protected static int mapActions(CnosDBGlobalState globalState, Action a) {
-        Randomly r = globalState.getRandomly();
-        int nrPerformed;
-        if (Objects.requireNonNull(a) == Action.INSERT) {
-            nrPerformed = r.getInteger(0, globalState.getOptions().getMaxNumberInserts());
-        } else {
-            throw new AssertionError(a);
-        }
-        return nrPerformed;
-
-    }
-
     @Override
     protected void checkViewsAreValid(CnosDBGlobalState globalState) {
     }
 
     @Override
     public void generateDatabase(CnosDBGlobalState globalState) throws Exception {
-        createTables(globalState, Randomly.fromOptions(4, 5, 6));
-        prepareTables(globalState);
-
+        // Table Creation (Creates Schema & Insert data into tables)
+        CnosDBTableCreator tableCreator = new CnosDBTableCreator(globalState);
+        tableCreator.create();
     }
 
     @Override
@@ -76,24 +54,6 @@ public class CnosDBProvider extends ProviderAdapter<CnosDBGlobalState, CnosDBOpt
         return connection;
     }
 
-    protected void createTables(CnosDBGlobalState globalState, int numTables) throws Exception {
-        while (globalState.getSchema().getDatabaseTables().size() < numTables) {
-            String tableName = String.format("m%d", globalState.getSchema().getDatabaseTables().size());
-            CnosDBOtherQuery createTable = CnosDBTableGenerator.generate(tableName);
-            globalState.executeStatement(createTable);
-        }
-    }
-
-    protected void prepareTables(CnosDBGlobalState globalState) throws Exception {
-        StatementExecutor<CnosDBGlobalState, Action> se = new StatementExecutor<>(globalState, Action.values(),
-                CnosDBProvider::mapActions, q -> {
-                    if (globalState.getSchema().getDatabaseTables().isEmpty()) {
-                        throw new IgnoreMeException();
-                    }
-                });
-        se.executeStatements();
-    }
-
     @Override
     public String getDBMSName() {
         return "CnosDB".toLowerCase();
@@ -102,22 +62,6 @@ public class CnosDBProvider extends ProviderAdapter<CnosDBGlobalState, CnosDBOpt
     @Override
     public LoggableFactory getLoggableFactory() {
         return new CnosDBLoggableFactory();
-    }
-
-    public enum Action implements AbstractAction<CnosDBGlobalState> {
-        INSERT(CnosDBInsertGenerator::insert);
-
-        private final CnosDBQueryProvider<CnosDBGlobalState> sqlQueryProvider;
-
-        Action(CnosDBQueryProvider<CnosDBGlobalState> sqlQueryProvider) {
-            this.sqlQueryProvider = sqlQueryProvider;
-        }
-
-        @Override
-        public CnosDBOtherQuery getQuery(CnosDBGlobalState state) throws Exception {
-            return new CnosDBOtherQuery(sqlQueryProvider.getQuery(state).getQueryString(),
-                    CnosDBExpectedError.expectedErrors());
-        }
     }
 
 }
---
+++ src/sqlancer/cnosdb/CnosDBTableQueryGenerator.java
@@ -0,0 +1,84 @@
+package sqlancer.cnosdb;
+
+import sqlancer.AbstractAction;
+import sqlancer.Randomly;
+import sqlancer.cnosdb.gen.CnosDBInsertGenerator;
+import sqlancer.cnosdb.query.CnosDBOtherQuery;
+import sqlancer.cnosdb.query.CnosDBQueryProvider;
+import sqlancer.common.TableQueryGenerator;
+
+import java.util.Objects;
+
+public class CnosDBTableQueryGenerator implements TableQueryGenerator {
+    public enum Action implements AbstractAction<CnosDBGlobalState> {
+        INSERT(CnosDBInsertGenerator::insert);
+
+        private final CnosDBQueryProvider<CnosDBGlobalState> sqlQueryProvider;
+
+        Action(CnosDBQueryProvider<CnosDBGlobalState> sqlQueryProvider) {
+            this.sqlQueryProvider = sqlQueryProvider;
+        }
+
+        @Override
+        public CnosDBOtherQuery getQuery(CnosDBGlobalState state) throws Exception {
+            return new CnosDBOtherQuery(sqlQueryProvider.getQuery(state).getQueryString(),
+                    CnosDBExpectedError.expectedErrors());
+        }
+    }
+
+    private final CnosDBGlobalState globalState;
+    private int total;
+    private int[] nrActions;
+
+    public CnosDBTableQueryGenerator(CnosDBGlobalState globalState) {
+        this.globalState = globalState;
+        this.total = 0;
+        this.nrActions = new int[Action.values().length];
+    }
+
+    protected int mapActions(Action a) {
+        Randomly r = globalState.getRandomly();
+        int nrPerformed;
+        if (Objects.requireNonNull(a) == Action.INSERT) {
+            nrPerformed = r.getInteger(0, globalState.getOptions().getMaxNumberInserts());
+        } else {
+            throw new AssertionError(a);
+        }
+        return nrPerformed;
+    }
+
+    private void generateNrActions() {
+        for (Action action : Action.values()) {
+            int nrPerformed = mapActions(action);
+            nrActions[action.ordinal()] = nrPerformed;
+            total += nrPerformed;
+        }
+    }
+
+    @Override
+    public void generate() {
+        generateNrActions();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return total == 0;
+    }
+
+    @Override
+    public Action getRandNextAction() {
+        int selection = globalState.getRandomly().getInteger(0, total);
+        int previousRange = 0;
+        for (int i = 0; i < nrActions.length; i++) {
+            if (previousRange <= selection && selection < previousRange + nrActions[i]) {
+                assert nrActions[i] > 0;
+                nrActions[i]--;
+                total--;
+                return Action.values()[i];
+            } else {
+                previousRange += nrActions[i];
+            }
+        }
+        throw new AssertionError();
+    }
+}
