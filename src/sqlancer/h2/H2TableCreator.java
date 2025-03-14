9a2b50d70c513177acf23d18f5414a3cb2ff0b0e
Zhai Yuxin <e0772744@u.nus.edu>
2025-03-14 16:48:32 +0800

    del commented lines

--- src/sqlancer/h2/H2TableCreator.java
+++ src/sqlancer/h2/H2TableCreator.java
@@ -51,14 +51,10 @@ public class H2TableCreator extends TableCreator {
             }
             if (query != null && query.couldAffectSchema()) {
                 globalState.updateSchema();
-                // throw new IgnoreMeException();
                 if (globalState.getSchema().getDatabaseTables().isEmpty()) {
                     throw new IgnoreMeException();
                 }
             }
-            // if (globalState.getSchema().getDatabaseTables().isEmpty()) {
-            // throw new IgnoreMeException();
-            // }
         }
     }
 
