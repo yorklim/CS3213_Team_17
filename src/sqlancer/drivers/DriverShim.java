acfd9fa10b7039d4cdad69452f91af2667b88e07
Jeremy <e1155908@u.nus.edu>
2025-03-30 22:02:18 +0800

    checkstyle fixes

--- src/sqlancer/drivers/DriverShim.java
+++ src/sqlancer/drivers/DriverShim.java
@@ -15,30 +15,37 @@ public class DriverShim implements Driver {
         this.driver = d;
     }
 
+    @Override
     public boolean acceptsURL(String u) throws SQLException {
         return this.driver.acceptsURL(u);
     }
 
+    @Override
     public Connection connect(String u, Properties p) throws SQLException {
         return this.driver.connect(u, p);
     }
 
+    @Override
     public DriverPropertyInfo[] getPropertyInfo(String u, Properties p) throws SQLException {
         return this.driver.getPropertyInfo(u, p);
     }
 
+    @Override
     public int getMajorVersion() {
         return this.driver.getMajorVersion();
     }
 
+    @Override
     public int getMinorVersion() {
         return this.driver.getMinorVersion();
     }
 
+    @Override
     public boolean jdbcCompliant() {
         return this.driver.jdbcCompliant();
     }
 
+    @Override
     public Logger getParentLogger() throws SQLFeatureNotSupportedException {
         return this.driver.getParentLogger();
     }
